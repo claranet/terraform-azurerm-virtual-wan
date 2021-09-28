3760c5f07dc339ec72df0e989b10739826de4f3d
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2021-09-28 11:26:32 +0200

    AZ-559 Fix example again again again again again again

---
+++ examples/main/versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = ">= 2.74"
+  }
+}
--- examples/vwan-with-er/main.tf
+++ examples/vwan-with-er/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    azurerm = ">= 2.74"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
