6e8b4e642c23826cfad3576987939a63dcbb907f
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2021-09-28 09:57:09 +0200

    AZ-559 Fix example

--- examples/main/variables.tf
+++ examples/main/variables.tf
@@ -1,39 +1,49 @@
 variable "client_name" {
-  type = string
+  description = "Name of client."
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Name of application's environment."
+  type        = string
 }
 
 variable "stack" {
-  type = string
+  description = "Name of application's stack."
+  type        = string
 }
 
 variable "azure_region" {
-  type = string
+  description = "Name of the Region."
+  type        = string
 }
 
 variable "log_analytics_workspace_name" {
-  type = string
+  description = "Name of the log Analytics Workspace."
+  type        = string
 }
 
 variable "vhub_address_prefix" {
-  type = string
+  description = "CIDR Range to use with the virtual hub. /24 mini, /23 recommended."
+  type        = string
 }
 
 variable "vnet_name" {
-  type = string
+  description = "Name of the Spoke virtual network"
+  type        = string
 }
 
 variable "vnet_address_space" {
-  type = list(string)
+  description = "List of CIDR ranges to use with the Spoke vnet."
+  type        = list(string)
 }
 
 variable "subnet_name" {
-  type = string
+  description = "Name of the subnet to deploy into the Spoke Vnet."
+  type        = string
 }
 
 variable "subnet_address_prefixes" {
-  type = list(string)
+  description = "List of Spoke subnet CIDR ranges."
+  type        = list(string)
 }
