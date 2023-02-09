6a6877b221f41335cf68a0a10fc295c0cf18df49
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2023-02-09 17:43:06 +0100

    AZ-994: Disallow null value for logs_destinations_ids

--- variables-logs.tf
+++ variables-logs.tf
@@ -7,6 +7,10 @@ List of destination resources IDs for logs diagnostic destination.
 Can be `Storage Account`, `Log Analytics Workspace` and `Event Hub`. No more than one of each can be set.
 If you want to specify an Azure EventHub to send logs and metrics to, you need to provide a formated string with both the EventHub Namespace authorization send ID and the EventHub name (name of the queue to use in the Namespace) separated by the `|` character.
 EOD
+  validation {
+    condition     = var.logs_destinations_ids != null
+    error_message = "To disable logging use an empty list ( [] ) instead of a null value."
+  }
 }
 
 variable "logs_retention_days" {
