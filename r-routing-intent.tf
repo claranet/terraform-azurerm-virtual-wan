3c3212aeaebb68da91f246b9186723eaeb1f5da4
Jeremy MARMOL <jeremy.marmol@fr.clara.net>
2023-06-16 16:48:38 +0200

    AZ-1091: Apply suggestions

--- r-routing-intent.tf
+++ r-routing-intent.tf
@@ -7,18 +7,19 @@ module "routing" {
 
   internet_routing_enabled = var.internet_routing_enabled
   private_routing_enabled  = var.private_routing_enabled
+  depends_on               = [null_resource.routing_precondition]
 }
 
 resource "null_resource" "routing_precondition" {
-  for_each = var.routing_intent_enabled ? toset(["enabled"]) : toset([])
+  for_each = toset(var.routing_intent_enabled ? ["enabled"] : [])
   triggers = {
     azfw_next_hop = var.azure_firewall_as_next_hop_enabled
     nexhop_nva_id = var.next_hop_nva_id
   }
   lifecycle {
     precondition {
-      condition     = ((var.azure_firewall_as_next_hop_enabled && var.next_hop_nva_id == null) || (!var.azure_firewall_as_next_hop_enabled && var.next_hop_nva_id != null))
-      error_message = "The varialbe 'next_hop_nva_id' must be null if 'azure_firewall_as_next_hop_enabled' is true or must not be emtpy if 'azure_firewall_as_next_hop_enabled' is false."
+      condition     = var.azure_firewall_as_next_hop_enabled ? var.next_hop_nva_id == null : var.next_hop_nva_id != null
+      error_message = "The variable 'next_hop_nva_id' must be null if 'azure_firewall_as_next_hop_enabled' is true or must not be empty if 'azure_firewall_as_next_hop_enabled' is false."
     }
   }
 }
